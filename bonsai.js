23e7391c970170e6d6d50d05a078528a5175ac87
DexCodeSX <176408964+DexCodeSX@users.noreply.github.com>
2026-04-23 10:27:23 -0700

    release: v2.5.10

--- bonsai.js
+++ bonsai.js
@@ -13,7 +13,7 @@ import https from 'https';
 import http from 'http';
 import crypto from 'crypto';
 
-const VERSION = "2.5.9";
+const VERSION = "2.5.10";
 const REPO = "DexCodeSX/Secret";
 const REPO_RAW = `https://raw.githubusercontent.com/${REPO}/main`;
 const isWin = process.platform === 'win32';
